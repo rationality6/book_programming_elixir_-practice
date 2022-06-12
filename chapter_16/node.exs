803ba3e93722ad37179282b20bf5050b3afb62bf
Hyun Ahn <codepawn@icloud.com>
2022-06-12 17:34:17 +0900

    added ticker and etc

---
+++ chapter_16/node.exs
@@ -0,0 +1,12 @@
+# iex -sname node_one --cookie chocolate-chip
+# iex -sname node_two --cookie chocolate-chip
+
+# Node.self
+
+# Node.connect("")
+
+# Node.list
+
+# func = fn -> IO.puts(Enum.join(File.ls!," ,")) end
+
+# Node.spawn(:"node_two@Hyuns-MacBook-Air", func)
