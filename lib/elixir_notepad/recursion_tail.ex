bed80f7d8a4a40cc26dd7bd306e560dd670bb7e4
Hyun Ahn <codepawn@icloud.com>
2022-07-08 16:30:19 +0900

    added test expriment

--- lib/elixir_notepad/recursion_tail.ex
+++ lib/elixir_notepad/recursion_tail.ex
@@ -1,13 +1,7 @@
-
-defmodule
-
-defmodule Recursion do
+defmodule RecursionTail do
   def factorial(1), do: 1
 
   def factorial(num) when is_integer(num) do
-    IO.inspect(num)
     num + factorial(num - 1)
   end
 end
-
-Recursion.factorial(5) |> IO.inspect()
