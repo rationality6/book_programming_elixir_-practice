2fef763a3a767247238ca19b51dd8c98483f6d5f
Hyun Ahn <codepawn@icloud.com>
2022-07-18 23:06:08 +0900

    insertion_sort (#6)

---
+++ lib/algo/selection_sort2.exs
@@ -0,0 +1,42 @@
+defmodule SelectionSort do
+  def run(list) do
+    select(list, [])
+  end
+
+  def select([head | []], _result = acc) do
+    acc ++ [head]
+  end
+
+  def select(list, _result = acc) do
+    min = select_minimum(list)
+    select(List.delete(list, min), acc ++ [min])
+  end
+
+  def select_minimum([head | [second | []]]) do
+    min(head, second)
+  end
+
+  def select_minimum([head | [second | tail]]) do
+    select_minimum([min(head, second) | tail])
+  end
+
+  def smaller(a, b) do
+    cond do
+      a <= b ->
+        a
+
+      a > b ->
+        b
+    end
+  end
+end
+
+
+array0 =
+  1..10
+  |> Enum.to_list()
+  |> Enum.shuffle()
+  |> IO.inspect()
+
+SelectionSort.run(array0)
+|> IO.inspect()
