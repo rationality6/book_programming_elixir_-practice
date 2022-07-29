d7bb6fa0d221af8e8d3fc81013463578fadf032e
Hyun Ahn <codepawn@icloud.com>
2022-07-29 18:07:52 +0900

    added_one_question (#10)

---
+++ lib/algo/three_call_funtion.exs
@@ -0,0 +1,17 @@
+# DESCRIPTION:
+# Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
+
+# When bool is truth-ish, func1 should be called, otherwise call the func2.
+defmodule Result do
+  def _if(bool, func1, func2) do
+    cond do
+      bool == true ->
+        func1.()
+      bool == false ->
+        func2.()
+    end
+  end
+end
+
+Result._if(true, fn -> IO.puts("true") end, fn -> IO.puts("false") end)
+Result._if(false, fn -> IO.puts("true") end, fn -> IO.puts("false") end)
